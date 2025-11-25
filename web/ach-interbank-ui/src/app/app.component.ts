d831f6be1062dfaf31c174d80aca304dc83c01d9
checho <chechoz20@gmail.com>
2025-11-25 12:46:08 -0500

    Ajuste test transactions

--- web/ach-interbank-ui/src/app/app.component.ts
+++ web/ach-interbank-ui/src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+// Ajusta las rutas si cambian, pero la idea es esta:
+import { NotificationContainerComponent } from './shared/components/notification-container.component';
+import { LoadingOverlayComponent } from './shared/components/loading-overlay.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NotificationContainerComponent,
+    LoadingOverlayComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
