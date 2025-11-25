0665f68167a52bb08098623f305e784e2d8d33f4
Sergio Andres Zapata <chechoz20@gmail.com>
2025-11-25 16:16:06 -0500

    Update transactions menu link

--- web/ach-interbank-ui/src/app/layout/main-layout.component.ts
+++ web/ach-interbank-ui/src/app/layout/main-layout.component.ts
@@ -48,7 +48,7 @@ export class MainLayoutComponent implements OnInit, OnDestroy {
     { label: 'Catálogos', route: '/catalogs', icon: 'inventory', permissions: ['CanReadCatalogs'] },
     {
       label: 'Transacciones',
-      route: '/transactions/create',
+      route: '/transactions',
       icon: 'swap_horiz',
       roles: ['Admin', 'ACH.Operator'],
       permissions: ['CanManageAch', 'CanReadAch']
