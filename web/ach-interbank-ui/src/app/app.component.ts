80f2d7b7847839917537628ffbec3bef615f7a78
Sergio Andres Zapata <chechoz20@gmail.com>
2025-11-24 22:31:13 -0500

    Fix root module imports to resolve Angular compile errors

--- web/ach-interbank-ui/src/app/app.component.ts
+++ web/ach-interbank-ui/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  readonly title = 'Gestión de Transacciones ACH';
+  title = 'ACH Interbank SPA';
 }
--- web/ach-interbank-ui/src/app/core/core.module.ts
+++ web/ach-interbank-ui/src/app/core/core.module.ts
@@ -1,34 +1,20 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+
 import { NotificationContainerComponent } from '../shared/components/notification-container.component';
 import { LoadingOverlayComponent } from '../shared/components/loading-overlay.component';
-
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, HttpClientModule, RouterModule, NotificationContainerComponent, LoadingOverlayComponent],
+  imports: [CommonModule, HttpClientModule, NotificationContainerComponent, LoadingOverlayComponent],
   exports: [NotificationContainerComponent, LoadingOverlayComponent],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoadingInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorInterceptor,
-      multi: true
-    }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ]
 })
 export class CoreModule {
