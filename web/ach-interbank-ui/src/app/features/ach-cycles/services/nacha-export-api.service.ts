eec319a97bc7f54b286dbbcb86da3a17b7984302
Sergio Andres Zapata <chechoz20@gmail.com>
2025-12-08 18:57:49 -0500

    Handle NACHA export list responses

--- web/ach-interbank-ui/src/app/features/ach-cycles/services/nacha-export-api.service.ts
+++ web/ach-interbank-ui/src/app/features/ach-cycles/services/nacha-export-api.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { ExportableAchCycle } from '../models/ach-cycle-export.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class NachaExportApiService {
@@ -10,7 +11,9 @@ export class NachaExportApiService {
   private readonly apiBaseUrl = environment.apiBaseUrl.replace(/\/+$/, '');
 
   getExportableCycles(): Observable<ExportableAchCycle[]> {
-    return this.http.get<ExportableAchCycle[]>(`${this.apiBaseUrl}/ach-cycles/exportable`);
+    return this.http
+      .get<ExportableAchCycle[] | { items?: ExportableAchCycle[] }>(`${this.apiBaseUrl}/ach-cycles/exportable`)
+      .pipe(map((response) => (Array.isArray(response) ? response : response?.items ?? [])));
   }
 
   downloadFile(cycleId: number): Observable<HttpResponse<Blob>> {
