b9610955ec3b26532e2979d8cb861763fdc40c08
Sergio Andres Zapata <chechoz20@gmail.com>
2026-01-21 23:08:24 -0500

    Update production API base URL

--- web/ach-interbank-ui/src/environments/environment.prod.ts
+++ web/ach-interbank-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:8081',
+  apiBaseUrl: 'http://cfaach.ddns.net:744',
   authEndpoint: 'auth',
   requestTimeoutMs: 15000,
   appVersion: '0.1.0'
