6441e24f97042350f4fafe756778bc0a0d1e18fb
checho <chechoz20@gmail.com>
2026-01-31 23:12:54 -0500

    API base URL usage

--- web/ach-interbank-ui/src/environments/environment.prod.ts
+++ web/ach-interbank-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://cfaach.ddns.net:8080',
+  apiBaseUrl: 'http://cfaach.ddns.net:744',
   authEndpoint: 'auth',
   requestTimeoutMs: 15000,
   appVersion: '0.1.0'
