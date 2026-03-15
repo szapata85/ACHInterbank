128af5bf7d119616daebe0e0a45eb49818500b7c
Sergio Andres Zapata <chechoz20@gmail.com>
2026-03-15 17:17:34 -0500

    Fix MapperConfiguration initialization for AutoMapper 16

--- src/Cfa.ACHInterbank.Application/Configuration/MapperProfile.cs
+++ src/Cfa.ACHInterbank.Application/Configuration/MapperProfile.cs
@@ -26,7 +26,7 @@ public static class MapperBootstrapper
             var configurationExpression = new MapperConfigurationExpression();
             configurationExpression.AddProfile<MapperProfile>();
 
-            var config = new MapperConfiguration(configurationExpression);
+            var config = new MapperConfiguration(configurationExpression, loggerFactory: null);
             _instance = config.CreateMapper();
         }
     }
