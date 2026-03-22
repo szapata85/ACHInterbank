8902a1875fdc592f7fbfca7f2e26e6e6272ce89f
checho <chechoz20@gmail.com>
2026-03-22 14:34:50 -0500

    Actualizacion de dependencias

--- src/Cfa.ACHInterbank.Persistence/ACH/Services/Implementation/AchReturnsService.cs
+++ src/Cfa.ACHInterbank.Persistence/ACH/Services/Implementation/AchReturnsService.cs
@@ -1,7 +1,7 @@
 using Cfa.ACHInterbank.Application.ACH.Interfaces;
 using Cfa.ACHInterbank.Application.ACH.Models;
-using Cfa.ACHInterbank.Domain.Entities.Transactions.Enums;
 using Cfa.ACHInterbank.Domain.Models.ACH;
+using Cfa.ACHInterbank.Domain.Models.Configurations;
 using Cfa.ACHInterbank.Persistence.DataBase;
 using Microsoft.EntityFrameworkCore;
 using System.Text;
