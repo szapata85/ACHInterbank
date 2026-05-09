c6cb204159fa99f5584861ccd77dff6bbabdcf82
Sergio Andres Zapata <chechoz20@gmail.com>
2026-05-09 13:46:57 -0500

    application: registrar servicios de procesamiento de respuestas ACH

---
+++ tests/Cfa.ACHInterbank.Tests/AchResponseDependencyInjectionTests.cs
@@ -0,0 +1,69 @@
+using Cfa.ACHInterbank.Application;
+using Cfa.ACHInterbank.Application.ACH.Responses.Homologation.Interfaces;
+using Cfa.ACHInterbank.Application.ACH.Responses.Processing.Interfaces;
+using Cfa.ACHInterbank.Application.ACH.Responses.Processing.Validation;
+using Cfa.ACHInterbank.Application.ACH.Responses.Repositories;
+using Cfa.ACHInterbank.Application.ACH.Responses.Processing.Services;
+using Cfa.ACHInterbank.Application.DataBase;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Cfa.ACHInterbank.Tests;
+
+public class AchResponseDependencyInjectionTests
+{
+    [Fact]
+    public void AddApplication_ShouldRegisterProcesarRespuestaAchCommandValidator()
+    {
+        var services = new ServiceCollection();
+        services.AddApplication();
+        var sp = services.BuildServiceProvider();
+
+        var validator = sp.GetService<ProcesarRespuestaAchCommandValidator>();
+
+        Assert.NotNull(validator);
+    }
+
+    [Fact]
+    public void AddApplication_ShouldRegisterProcesarRespuestaAchUseCaseDependencies()
+    {
+        var services = new ServiceCollection();
+        services.AddApplication();
+        services.AddSingleton(Mock.Of<IAchResponseRepository>());
+        services.AddSingleton(Mock.Of<IAchResponseNotificationAttemptRepository>());
+        services.AddSingleton(Mock.Of<IRespuestaAchStatusMappingService>());
+        services.AddSingleton(Mock.Of<IUnitOfWork>());
+        var sp = services.BuildServiceProvider();
+
+        var sut = sp.GetService<IProcesarRespuestaAchUseCase>();
+
+        Assert.NotNull(sut);
+    }
+
+    [Fact]
+    public void AddApplication_ShouldRegisterAchResponseIdempotencyHashService()
+    {
+        var services = new ServiceCollection();
+        services.AddApplication();
+        var sp = services.BuildServiceProvider();
+
+        var svc = sp.GetService<IAchResponseIdempotencyHashService>();
+
+        Assert.NotNull(svc);
+        Assert.IsType<AchResponseIdempotencyHashService>(svc);
+    }
+
+    [Fact]
+    public void AddApplication_ShouldRegisterRespuestaAchStatusMappingService()
+    {
+        var services = new ServiceCollection();
+        services.AddApplication();
+        services.AddSingleton(Mock.Of<IAchResponseStatusMappingRepository>());
+        var sp = services.BuildServiceProvider();
+
+        var svc = sp.GetService<IRespuestaAchStatusMappingService>();
+
+        Assert.NotNull(svc);
+    }
+}
